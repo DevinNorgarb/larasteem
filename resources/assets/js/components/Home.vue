15f001ee70c139cc01e3cec14f3726d517e0b95a
DevinNorgarb <dnorgarb@gmail.com>
2018-01-27 03:59:27 +0200

    Profile component

--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -1,12 +1,15 @@
 <template lang="html">
-
+<profile></profile>
 </template>
 
 <script>
 import steem from 'steem'
-
+import Profile from './profile/Profile.vue'
 export default {
   name: 'home',
+  components: {
+    Profile
+  },
   mounted() {
     console.log(steem);
   }
---
+++ resources/assets/js/components/profile/Profile.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+<div class="">
+  profile test
+</div>
+</template>
+
+<script>
+export default {
+  name: 'profile'
+
+}
+</script>
+
+<style lang="css">
+</style>
